3816a1c1747aa8e61d8e071956cc77b2f5cf9a3f
Your Name <your.email@example.com>
2026-08-08 11:55:37 +1100

    Add Notifiable trait to Admin model so password-reset notifications can be sent

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,12 +6,15 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class Admin extends Authenticatable
 {
+    use Notifiable;
+
     /**
      * Get the attributes that should be cast.
      *
